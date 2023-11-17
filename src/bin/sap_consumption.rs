2c02e11e41e150feb5a1f68cb69537a880ab35e1
paddymills <paddymills@pm.me>
2023-11-17 16:21:16 -0500

    almost finished

--- src/bin/sap_consumption.rs
+++ src/bin/sap_consumption.rs
@@ -1,27 +1,60 @@
 
 use chrono::{Duration, Local, NaiveDateTime, NaiveTime, Timelike};
+use std::fs::File;
+use std::io::Write;
+
 use sysinteg::config::DbConnParams;
+use sysinteg::db;
+
+// TODO: store last queried time in database
+const INTERVAL: i64 = 1;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
 
     let now = Local::now();
     let end = NaiveDateTime::new(now.date_naive(), NaiveTime::from_hms_opt(now.hour(), 0, 0).unwrap());
-    let start = end - Duration::hours(1);
+    let start = end - Duration::hours(INTERVAL);
 
     println!("pulling data for duration [{}, {})", start.format("%d/%m/%Y %H:%M"), end.format("%d/%m/%Y %H:%M"));
 
-    let mut client = DbConnParams::load()?.connect().await?;
-    client
-        .query(
-            "EXECUTE SapProductionData @Start=@P1, @End=@P2",
-            &[&start, &end]
-        )
-            .await?
-        .into_first_result()
-            .await?
-        .iter()
-        .for_each(|row| println!("{:?}", row));
+    let mut client = DbConnParams::load()?
+        .connect()
+            .await?;
+    
+    let prod = client
+        .query("EXEC SapProduction @Start=@P1, @End=@P2", &[&start, &end]).await?
+        .into_first_result().await?;
+
+    let issue = client
+        .query("EXEC SapIssueData @Start=@P1, @End=@P2", &[&start, &end]).await?
+        .into_first_result().await?;
+
+    write_data(prod, "Production", end);
+    write_data(issue, "Issue", end);
 
     Ok(())
+}
+
+fn write_data(dataset: Vec<tiberius::Row>, name: &str, timestamp: NaiveDateTime) {
+    // TODO: store on server
+    let filename = format!("{}_{}.ready", name, timestamp.format("%Y%m%d%H%M%S"));
+    let mut file = File::create(&filename)
+        .expect(&format!("Failed to create file {}", &filename));
+
+    if dataset.len() == 0 {
+        println!("Dataset {} is empty", name);
+    } else {
+        println!("Writing dataset {}", name);
+        dataset
+            .into_iter()
+            // convert row to tab delimited string
+            .map(|row| db::row_to_string(row))
+            .for_each(|row| {
+                file.write_all(row.as_bytes())
+                    .expect("Failed to write data to file");
+                file.write_all(b"\n")
+                    .expect("failed to write newline character");
+                });
+    }
 }
