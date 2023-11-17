a5d08b56f5711f7efbea37bcedd5031af5b9b97d
paddymills <paddymills@pm.me>
2023-11-17 16:06:33 -0500

    add string parsing functions

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 
 //! database connections
 
-use tiberius::{AuthMethod, Client, Config, error::Error};
+use tiberius::{AuthMethod, Client, Config, error::Error, ColumnData, Row};
 use tokio::net::TcpStream;
 use tokio_util::compat::{Compat, TokioAsyncWriteCompatExt};
 
@@ -27,5 +27,36 @@ pub async fn connect(host: &str, database: &str) -> Result<DbClient, Error> {
     let client = Client::connect(config, tcp.compat_write()).await?;
 
     Ok(client)
+}
+
+/// convert SQL Server column to string, regardless of datatype
+pub fn column_to_str(column: ColumnData) -> String {
+    use ColumnData::*;
+    match column {
+        U8(n)  => n.unwrap().to_string(),
+        I16(n) => n.unwrap().to_string(),
+        I32(n) => n.unwrap().to_string(),
+        I64(n) => n.unwrap().to_string(),
+        F32(n) => n.unwrap().to_string(),
+        F64(n) => n.unwrap().to_string(),
+        Numeric(n) => n.unwrap().to_string(),
+
+        String(s) => s.unwrap().to_string(),
+
+        _ => unimplemented!()
+    }
+}
+
+/// Converts a SQL row to a tab-delimited string
+pub fn row_to_string(row: Row) -> String {
+    row.into_iter()
+        .map(column_to_str)
+
+        // generally speaking, one the these lines should be no more than 128 characters
+        //  so we set an initial capacity of 128 to try to avoid reallocations
+        .fold(String::with_capacity(128), |acc, s| acc + &s + "\t")
+        
+        .trim_end() // remove trailing '\t'
+        .into()
 }
 
