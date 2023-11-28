e2ff74f3f2710bde231a352af02ba06a69a8314a
Patrick Miller <paddymills@proton.me>
2023-11-28 09:33:10 -0500

    impl dataset enum

---
+++ src/bin/sap_consumption/dataset.rs
@@ -0,0 +1,79 @@
+
+use std::fs::File;
+use std::path::PathBuf;
+use std::io::Write;
+
+use tiberius::{Query, Result};
+use sysinteg::db::{self, DbClient};
+
+
+pub enum Dataset {
+    Production,
+    Issue
+}
+
+impl Dataset {
+    fn name(&self) -> &str {
+        match self {
+            Self::Production => "Production",
+            Self::Issue => "Issue"
+        }
+    }
+
+    fn query(&self) -> Query {
+        let sql = match self {
+            Self::Production => "EXEC SapProductionData_SinceLastRun",
+            Self::Issue      => "EXEC SapIssueData_SinceLastRun"
+        };
+
+        Query::new(sql)
+    }
+
+    fn filename(&self, end: chrono::NaiveDateTime, output_dir: &PathBuf) -> PathBuf {
+        let filename = format!("{}_{}.ready", self.name(), end.format("%Y%m%d%H%M%S"));
+        let filename = output_dir.join(filename);
+
+        filename
+    }
+
+    pub async fn pull_data(self, client: &mut DbClient, end: chrono::NaiveDateTime, output_dir: &PathBuf) -> Result<()> {
+        let name = self.name();
+
+        log::trace!("pulling {} dataset", name);
+        let data = self.query().query(client).await?
+            .into_first_result().await?;
+
+        if data.len() == 0 {
+            log::debug!("Dataset {} is empty", name);
+        } else {
+            // TODO: store on server for verification once feedback loop from SAP is established
+            let filename = self.filename(end, output_dir);
+            let mut file = File::create(&filename)
+                .map_err(|error| {
+                    log::error!("Failed to create {} file {}", name, &filename.to_str().unwrap());
+                
+                    error
+                })?;
+        
+            log::trace!("Writing dataset {}", name);
+            let file_contents = data
+                .into_iter()
+                // convert row to tab delimited string
+                .map(|row| db::row_to_string(row))
+                .collect::<Vec<String>>()
+                .join("\n");
+    
+            file.write_all(file_contents.as_bytes())
+                .map_err(|error| {
+                    log::error!("failed to write {} dataset to {}. Deleting file.", name, &filename.to_str().unwrap());
+                    let _ = std::fs::remove_file(filename);
+
+                    error
+                })?;
+
+            client.execute("UPDATE HighSteel.RuntimeInfo SET last_runtime=@P1 WHERE name=@P2", &[&end, &format!("Sap{}Data", name)]).await?;
+        }
+    
+        Ok(())
+    }
+}
--- src/bin/sap_consumption/main.rs
+++ src/bin/sap_consumption/main.rs
@@ -4,20 +4,15 @@
 
 mod cli;
 mod config;
+mod dataset;
 
-use chrono::{Duration, Local, NaiveDateTime, NaiveTime, Timelike};
-use config::{CONFIG_FILE, SapConsumptionConfig};
-
-use std::fs::File;
-use std::path::PathBuf;
-use std::io::Write;
-
+use chrono::{Local, NaiveDateTime, NaiveTime, Timelike};
 use clap::Parser;
+use std::path::PathBuf;
 
-use sysinteg::{config::TomlConfig, db};
-
-// TODO: store last queried time in database
-const INTERVAL: i64 = 1;
+use config::{CONFIG_FILE, SapConsumptionConfig};
+use sysinteg::config::TomlConfig;
+use dataset::Dataset;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -39,51 +34,12 @@ async fn main() -> anyhow::Result<()> {
 async fn pull_interval(config: SapConsumptionConfig) -> anyhow::Result<()> {
     let now = Local::now();
     let end = NaiveDateTime::new(now.date_naive(), NaiveTime::from_hms_opt(now.hour(), 0, 0).unwrap());
-    let start = end - Duration::hours(INTERVAL);
 
-    log::info!("pulling data for duration [{}, {})", start.format("%d/%m/%Y %H:%M"), end.format("%d/%m/%Y %H:%M"));
+    log::info!("pulling data from last run until {}", end.format("%d/%m/%Y %H:%M"));
 
     let mut client = config.database.connect().await?;
-    
-    log::trace!("pulling Production dataset");
-    let prod = client
-        .query("EXEC SapProductionData @Start=@P1, @End=@P2", &[&start, &end]).await?
-        .into_first_result().await?;
-
-        
-    log::trace!("pulling Issue dataset");
-    let issue = client
-        .query("EXEC SapIssueData @Start=@P1, @End=@P2", &[&start, &end]).await?
-        .into_first_result().await?;
-
-    write_data(prod,  "Production", end, &config.output_dir);
-    write_data(issue, "Issue",      end, &config.output_dir);
+    Dataset::Production.pull_data(&mut client, end, &config.output_dir).await?;
+    Dataset::Issue.pull_data(&mut client, end, &config.output_dir).await?;
 
     Ok(())
-}
-
-fn write_data(dataset: Vec<tiberius::Row>, name: &str, timestamp: NaiveDateTime, outdir: &PathBuf) {
-    if dataset.len() == 0 {
-        log::debug!("Dataset {} is empty", name);
-    } else {
-        // TODO: store on server
-        let filename = format!("{}_{}.ready", name, timestamp.format("%Y%m%d%H%M%S"));
-        let filename = outdir.join(filename);
-        
-        let mut file = File::create(&filename)
-            .map_err(|_| log::error!("Failed to create file {}", &filename.to_str().unwrap()))
-            .unwrap();
-    
-        log::info!("Writing dataset {}", name);
-        dataset
-            .into_iter()
-            // convert row to tab delimited string
-            .map(|row| db::row_to_string(row))
-            .for_each(|row| {
-                file.write_all(row.as_bytes())
-                    .expect("Failed to write data to file");
-                file.write_all(b"\n")
-                    .expect("failed to write newline character");
-                });
-    }
 }
