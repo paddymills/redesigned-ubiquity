88b41ded7d13637f7168f7216f902ec09f86e116
Patrick Miller <paddymills@proton.me>
2023-12-01 00:16:23 -0500

    remove terminal feature

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,6 @@
 #![warn(missing_docs)]
 
 //! High Steel system integration components
-//! 
-//! features (none are default)
-//!   - `terminal` enables logging to [`std::io::stdout`]
 
 pub mod config;
 pub mod db;
